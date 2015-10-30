b2eaae16722a0fe2388d4f8a5ac30296256ea704
Carsten Thiel <cat@frejbow.net>
2015-10-30 19:22:57 +0100

    upgrade puppet modules and adapt

--- puppet/modules/software/manifests/aptconfig.pp
+++ puppet/modules/software/manifests/aptconfig.pp
@@ -1,32 +1,46 @@
 class software::aptconfig {
 
   class { 'apt':
-    always_apt_update    => true,
+    update      => {
+      frequency => 'always',
+    },
   }
 
   apt::source { 'owncloud':
     location    => 'http://download.opensuse.org/repositories/isv:/ownCloud:/desktop/xUbuntu_14.04/',
     release     => '',
     repos       => '/',
-    include_src => false,
-    key         => 'F9EA4996747310AE79474F44977C43A8BA684223',
-    key_source  => 'http://download.opensuse.org/repositories/isv:ownCloud:desktop/xUbuntu_14.04/Release.key',
+    key         => {
+      'id'      =>  'F9EA4996747310AE79474F44977C43A8BA684223',
+      'source'  => 'http://download.opensuse.org/repositories/isv:ownCloud:desktop/xUbuntu_14.04/Release.key',
+    },
+    include  => {
+      'src' => false,
+    },
   }
 
   apt::source { 'google-earth':
-    location    => 'http://dl.google.com/linux/earth/deb/',
-    release     => 'stable',
-    repos       => 'main',
-    include_src => false,
-    key         => '4CCA1EAF950CEE4AB83976DCA040830F7FAC5991',
+    location => 'http://dl.google.com/linux/earth/deb/',
+    release  => 'stable',
+    repos    => 'main',
+    key      => {
+      'id'   => '4CCA1EAF950CEE4AB83976DCA040830F7FAC5991',
+    },
+    include  => {
+      'src'  => false,
+    },
   }
 
   apt::source { 'dropbox':
     location    => 'http://linux.dropbox.com/ubuntu',
     release     => 'trusty',
     repos       => 'main',
-    include_src => false,
-    key         => '1C61A2656FB57B7E4DE0F4C1FC918B335044912E',
+    key         => {
+      'id'      =>  '1C61A2656FB57B7E4DE0F4C1FC918B335044912E',
+    },
+    include  => {
+      'src' => false,
+    },
   }
 
   apt::ppa { 'ppa:stebbins/handbrake-snapshots': }
@@ -34,11 +48,15 @@ class software::aptconfig {
   apt::ppa { 'ppa:pidgin-gnome-keyring/ppa': }
 
   apt::source { 'videolan':
-    location    => 'http://download.videolan.org/pub/debian/stable/',
-    release     => '',
-    repos       => '/',
-    include_src => false,
-    key         => '8F0845FE77B16294429A79346BCA5E4DB84288D9',
+    location => 'http://download.videolan.org/pub/debian/stable/',
+    release  => '',
+    repos    => '/',
+    key      => {
+      'id'   => '8F0845FE77B16294429A79346BCA5E4DB84288D9',
+    },
+    include  => {
+      'src'  => false,
+    },
   }
 
 }
